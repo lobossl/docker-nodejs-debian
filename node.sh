21d26ac92eafa9ce1b5f6c96830e8b01bce988b3
Thomas <triohouse@hotmail.com>
2022-05-02 21:20:21 +0200

    update node.js
    fixed name from node to nodejs

--- node.sh
+++ node.sh
@@ -1,4 +1,4 @@
 #Installing debian image with NODE-JS
 sudo docker pull debian
 sudo docker run --name nodejs -it debian bash -c "apt -y update;apt -y upgrade;apt -y install nano nodejs npm"
-sudo docker commit node debian:nodejs
+sudo docker commit nodejs debian:nodejs
