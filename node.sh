240ae8ad54bcf18729db17120837b148896e7bd2
Thomas <triohouse@hotmail.com>
2022-05-02 21:29:05 +0200

    update on node.sh
    added sudo docker run -it debian:nodejs bash

--- node.sh
+++ node.sh
@@ -2,3 +2,4 @@
 sudo docker pull debian
 sudo docker run --name nodejs -it debian bash -c "apt -y update;apt -y upgrade;apt -y install nano nodejs npm"
 sudo docker commit nodejs debian:nodejs
+sudo docker run -it debian:nodejs bash
